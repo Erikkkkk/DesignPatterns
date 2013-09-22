938a8372321a6186ffcb1c50d393b7eb1e299c2f
erikkkk <ei.slim@st.hanze.nl>
2013-09-22 20:08:22 +0200

    Executor is Singleton

--- src/nl/hanze/designpatterns/db/Executor.java
+++ src/nl/hanze/designpatterns/db/Executor.java
@@ -1,8 +1,56 @@
 package nl.hanze.designpatterns.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.*;
 
 public class Executor {
+	private final String databaseDriver;
+	private final String databaseConnectionString;
+	
+	private Connection connect;
+	
+	private static Executor executor;
+	
+	private Executor() {
+		databaseDriver = "com.mysql.jdbc.Driver";
+		
+		String databaseURL = "jdbc:mysql:localhost";
+		String databaseName = "funda2";
+		String databaseUser = "root";
+		String databasePassword = "";
+		
+		databaseConnectionString = databaseDriver + "//" + databaseURL + "/" + databaseName + "?user=" +
+				databaseUser + "&password=" + databasePassword;
+	}
+	
+	public Executor getExecutor() {
+		
+		if(executor == null) {
+			executor = new Executor();
+			connectToMySQL();
+		}
+		
+		return executor;
+		
+	}
+	
+	private void connectToMySQL() {
+		try {
+			Class.forName(databaseDriver);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			connect = DriverManager.getConnection(databaseConnectionString);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
 	public void executeQuery(String query) throws Exception {
 	}
 	
